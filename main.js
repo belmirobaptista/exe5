6fb1915ac507aae998a09ec3d19c70a7b721b8aa
belmirobaptista <belmirobaptistab@gmail.com>
2024-07-31 20:00:50 +0100

    change

---
+++ main.js
@@ -0,0 +1,7 @@
+const navbar = document.querySelector(".navbar")
+const menubutton = document.querySelector(".menu-butter")
+menubutton.addEventListener('click', ()=>{
+     navbar.classList.toggle("show-menu")
+     
+
+})
